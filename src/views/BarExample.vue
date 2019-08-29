abc7e86300fcaa1b9218b2cc9a6a4f4089788405
Iana Kotreleva <iana.kotreleva@gmail.com>
2019-08-29 19:29:43 +0300

    Add empty page for bar example

---
+++ src/views/BarExample.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <Header />
+    <div class="text-center msg-block">
+      The example is going to be soon...
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '../components/header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+.msg-block {
+  font-size: 25px;
+  font-weight: 300;
+  margin-top: 100px;
+}
+</style>
+
+
