1d2d1ad81001ee0a1a11e63169ead74b8901021d
Iana Kotreleva <iana.kotreleva@gmail.com>
2019-10-29 15:03:52 +0100

    Add highlight code component

---
+++ src/components/HighlightCode.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Highlight :language="language">{{ text }}</Highlight>
+  </div>
+</template>
+
+<script>
+import Highlight from 'vue-highlight-component'
+
+export default {
+  props: {
+    language: {
+      type: String,
+      default: 'html'
+    },
+    text: String
+  },
+  components: {
+    Highlight
+  }
+}
+</script>
